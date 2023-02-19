f81e545fc8b4c8bd163c00ae2f7dcf4dcf90eb66
andreoneres <andreoliveiraneres@gmail.com>
2023-02-19 16:26:42 -0300

    test: adds holiday endpoint tests

---
+++ tests/Unit/Endpoints/HolidayTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+
+beforeEach(function () {
+    $this->mocks = [
+        "holiday" => new MockHandler([
+            new Response(200, [], $this->jsonMock("Holidays")),
+        ])
+    ];
+});
+
+test("should search all national holidays", function () {
+    $container = [];
+    $brasilApi = $this->buildClient($container, $this->mocks["holiday"]);
+    
+    $year = 2023;
+    $address = $brasilApi->holidays()->fromYear($year);
+    
+    expect($address)
+        ->toEqual($this->arrayMock("Holidays"));
+    
+    expect($this->getRequestUri($container[0]))
+        ->toBe("/api/feriados/v1/{$year}");
+    
+    expect($this->getRequestMethod($container[0]))
+        ->toBe("GET");
+});
