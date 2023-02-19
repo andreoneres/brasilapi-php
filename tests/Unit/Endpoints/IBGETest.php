9d994f4bb38556f3b448f5760ecab7a88aef8ead
andreoneres <andreoliveiraneres@gmail.com>
2023-02-19 16:44:37 -0300

    test: adds IBGE endpoint tests

---
+++ tests/Unit/Endpoints/IBGETest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+
+beforeEach(function () {
+    $this->mocks = [
+        "stateCities" => new MockHandler([
+            new Response(200, [], $this->jsonMock("IBGEStateCities")),
+        ]),
+        "states" => new MockHandler([
+            new Response(200, [], $this->jsonMock("IBGEStates")),
+        ]),
+        "state" => new MockHandler([
+            new Response(200, [], $this->jsonMock("IBGEState")),
+        ]),
+    ];
+});
+
+test("should search all cities of a specific state", function () {
+    $container = [];
+    $brasilApi = $this->buildClient($container, $this->mocks["stateCities"]);
+    
+    $uf = "AL";
+    $address = $brasilApi->ibge()->stateCities($uf);
+    
+    expect($address)
+        ->toEqual($this->arrayMock("IBGEStateCities"));
+    
+    expect($this->getRequestUri($container[0]))
+        ->toBe("/api/ibge/municipios/v1/{$uf}");
+    
+    expect($this->getRequestMethod($container[0]))
+        ->toBe("GET");
+});
+
+test("should search for information from all Brazilian states", function () {
+    $container = [];
+    $brasilApi = $this->buildClient($container, $this->mocks["states"]);
+    
+    $address = $brasilApi->ibge()->states();
+    
+    expect($address)
+        ->toEqual($this->arrayMock("IBGEStates"));
+    
+    expect($this->getRequestUri($container[0]))
+        ->toBe("/api/ibge/uf/v1");
+    
+    expect($this->getRequestMethod($container[0]))
+        ->toBe("GET");
+});
+
+test("should information from a specific state", function () {
+    $container = [];
+    $brasilApi = $this->buildClient($container, $this->mocks["state"]);
+    
+    $uf = "AL";
+    $address = $brasilApi->ibge()->state($uf);
+    
+    expect($address)
+        ->toEqual($this->arrayMock("IBGEState"));
+    
+    expect($this->getRequestUri($container[0]))
+        ->toBe("/api/ibge/uf/v1/{$uf}");
+    
+    expect($this->getRequestMethod($container[0]))
+        ->toBe("GET");
+});
